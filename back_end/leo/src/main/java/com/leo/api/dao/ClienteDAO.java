aa6f0d03279204a1ef342d735469951dfe28516a
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-08-27 18:34:19 -0300

    Criação do ClienteDAO

---
+++ back_end/leo/src/main/java/com/leo/api/dao/ClienteDAO.java
@@ -0,0 +1,60 @@
+package com.leo.api.dao;
+
+import java.util.Date;
+
+import com.leo.api.entities.Cliente;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Getter
+public class ClienteDAO {
+	
+	private Long id;
+	private String nome;
+	private String cpf;
+	private String email;
+	private Date data_nascimento;
+	private String nome_mae;
+
+	public ClienteDAO() {
+	}
+
+	public ClienteDAO(Cliente cliente) {
+		id = cliente.getId();
+		nome = cliente.getNome();
+		cpf = cliente.getCpf();
+		email = cliente.getEmail();
+		data_nascimento = cliente.getData_nascimento();
+		nome_mae = cliente.getNome_mae();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public String getNome_mae() {
+		return nome_mae;
+	}
+
+	
+}
--- back_end/leo/src/main/java/com/leo/api/entities/Cliente.java
+++ back_end/leo/src/main/java/com/leo/api/entities/Cliente.java
@@ -2,6 +2,7 @@ package com.leo.api.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,19 +12,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "clientes")
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-@Getter
-@Setter
 public class Cliente implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -48,5 +39,82 @@ public class Cliente implements Serializable{
 	
 	@Column(name = "nome_mae")
 	private String nome_mae;
-	
+
+	public Cliente() {
+	}
+
+	public Cliente(Long id, String nome, String cpf, String email, Date data_nascimento, String nome_mae) {
+		this.id = id;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.data_nascimento = data_nascimento;
+		this.nome_mae = nome_mae;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(Date data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
+
+	public String getNome_mae() {
+		return nome_mae;
+	}
+
+	public void setNome_mae(String nome_mae) {
+		this.nome_mae = nome_mae;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+
 }
