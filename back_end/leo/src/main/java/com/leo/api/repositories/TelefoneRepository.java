3f7a568035e044f3c8b7fd39524d6368a63d177b
LeoDiasCarvalho <leodiascarvalhosr@gmail.com>
2023-08-28 16:24:16 -0300

    Interface TelefoneRepository

---
+++ back_end/leo/src/main/java/com/leo/api/repositories/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.leo.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.leo.api.entities.Telefone;
+
+public interface TelefoneRepository extends JpaRepository<Telefone, Long>{
+
+}
